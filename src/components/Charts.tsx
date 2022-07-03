2c4f9cbe966d6fe788c52c11d48817918504e137
Pranshu1902 <pranshu1902@gmail.com>
2022-07-04 03:00:43 +0530

    added basic chart

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,19 +1,67 @@
 import { useEffect, useState } from "react";
 import { Audio } from "react-loader-spinner";
-import { me } from "../api/ApiUtils";
+import { getTransactions, me } from "../api/ApiUtils";
 import Dashboard from "./Dashboard";
+import { Chart } from "react-google-charts";
+import { transactionType } from "../types/DataTypes";
+// import Moment from "moment";
 
 export default function Charts() {
   const [user, setUser] = useState("");
   const [showDashboard, setShowDashboard] = useState(false);
+
+  let myData: any = [[]];
+  const [data, setData] = useState(myData);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     me().then((currentUser) => {
       setUser(currentUser.username);
     });
 
+    getTransactions().then((data) => {
+      let history: any[] = [];
+      let spent = 0,
+        gained = 0;
+      data.forEach((element: transactionType) => {
+        element.spent
+          ? (spent += Number(element.amount))
+          : (gained += Number(element.amount));
+
+        // element.spent
+        //   ? history.push([
+        //       Moment(element.time).format("d MMM YY"),
+        //       element.amount,
+        //       0,
+        //       -element.amount,
+        //     ])
+        //   : history.push([
+        //       Moment(element.time).format("d MMM YY"),
+        //       0,
+        //       element.amount,
+        //       element.amount,gained
+        //     ]);
+      });
+      let net = gained - spent;
+      history.push(["Today", spent, gained, net]);
+      let allData = [["Date", "Spent", "Received", "Net"], ...history];
+
+      setData(allData);
+      setLoading(false);
+    });
+
     document.title = "Chart | Money Manager";
   }, []);
+
+  // chart components
+
+  // https://github.com/RakanNimer/react-google-charts
+
+  const options = {
+    title: "Transactions History",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
 
   return (
     <div className="flex flex-col">
@@ -43,15 +91,26 @@ export default function Charts() {
           showCross={false}
         />
       </div>
+      {/* Main Div */}
       <div
         style={{ paddingLeft: "26%" }}
         className="p-6 bg-gray-200 w-full h-screen"
       >
         <div>
-          <p className="text-5xl font-bold text-purple-500">Charts</p>
+          <p className="text-5xl font-bold text-purple-500">Chart</p>
         </div>
-        <div className="w-full h-full flex justify-center items-center">
-          <Audio height="250" width="250" color="red" ariaLabel="loading" />
+        <div className="flex justify-center items-center w-full h-full">
+          {loading ? (
+            <Audio height="250" width="250" color="red" ariaLabel="loading" />
+          ) : (
+            <Chart
+              chartType="Bar"
+              data={data}
+              width="100%"
+              height="400px"
+              options={options}
+            />
+          )}
         </div>
       </div>
     </div>
